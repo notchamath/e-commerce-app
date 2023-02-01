88769cc2b86f79dbb0d3b2995b4d9ab0c13038c5
Chamath Jayarathne <chamathsocial@gmail.com>
2023-02-01 14:07:29 -0500

    feat: Finish products routes

--- server/controllers/productControllers.js
+++ server/controllers/productControllers.js
@@ -1,10 +1,16 @@
 const asyncHandler = require('express-async-handler');
+const Product = require('../models/productModel');
 
 // @desc    Get all products
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler( async (req, res) => {
-    res.status(200).json({msg:'GET'})
+
+    // get products from db
+    const products = await Product.find();
+
+    // response
+    res.status(200).json(products);
 });
 
 // @desc    Add a new product
@@ -12,11 +18,22 @@ const getProducts = asyncHandler( async (req, res) => {
 // @access  Private
 const addProducts = asyncHandler( async (req, res) => {
 
+    // check for empty fields
     if(!req.body.name || !req.body.price || !req.body.category || !req.body.image){
         res.status(400);
         throw new Error('Fill out all product details such as product name, price, category and image');
     }
-    res.status(200).json({msg:'ADD'})
+
+    // create product in db
+    const product = await Product.create({
+        name: req.body.name,
+        price: req.body.price,
+        category: req.body.category,
+        image: req.body.image
+    });
+
+    // response
+    res.status(200).json(product)
 });
 
 // @desc    Update an existing product
@@ -24,18 +41,54 @@ const addProducts = asyncHandler( async (req, res) => {
 // @access  Private
 const updateProduct = asyncHandler( async (req, res) => {
 
+    const productId = req.body.id;
+    const product = await Product.findById(productId);
+
+    // check if product exists in db
+    if(!product){
+        res.status(400);
+        throw new Error('Product with that ID was not found');
+    }
+
+    // check for empty fields
     if(!req.body.name || !req.body.price || !req.body.category || !req.body.image){
         res.status(400);
         throw new Error('Fill out all product details such as product name, price, category and image');
     }
-    res.status(200).json({msg:'UPDATE'})
+
+    // isolate fields needed for the product to be updated
+    const updateProduct = {
+        name: req.body.name,
+        price: req.body.price,
+        category: req.body.category,
+        image: req.body.image,
+    }
+
+    // update the product in db
+    const updatedProduct = await Product.findByIdAndUpdate(productId, updateProduct, {new: true});
+
+    // response
+    res.status(200).json(updatedProduct);
 });
 
 // @desc    Remove an existing product
 // @route   DELETE /api/products
 // @access  Private
 const removeProducts = asyncHandler( async (req, res) => {
-    res.status(200).json({msg:'REMOVE'})
+    const productId = req.body.id;
+    const product = await Product.findById(productId);
+
+    // check if product exists in db
+    if(!product){
+        res.status(400);
+        throw new Error('Product with that ID was not found');
+    }
+
+    // remove product from db
+    const removedProduct = await Product.findByIdAndDelete(productId);
+    
+    // response
+    res.status(200).json(removedProduct);
 });
 
 module.exports = {
---
+++ server/models/productModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add product name'],
+        unique: true
+    },
+    price: {
+        type: String,
+        required: [true, 'Please add product price']
+    },
+    category: {
+        type: String,
+        required: [true, 'Please add product category']
+    },
+    image: {
+        type: String,
+        required: [true, 'Please add product image']
+    },
+});
+
+module.exports = mongoose.model('Product', productSchema);
