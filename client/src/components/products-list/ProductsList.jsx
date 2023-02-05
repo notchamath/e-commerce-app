10f8228b2889c6e824de52fc44c46ea3ec91bec6
Chamath Jayarathne <chamathsocial@gmail.com>
2023-02-05 17:21:34 -0500

    feat: Create Admin search

--- client/src/components/products-list/ProductsList.jsx
+++ client/src/components/products-list/ProductsList.jsx
@@ -1,18 +1,44 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import ProductsItem from '../products-item/ProductsItem';
 
 export default function ProductsList() {
   const {products} = useSelector(state => state.products);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  }
+  
+  const filteredProducts = products.filter(product => {
+    return product.name.toLowerCase().includes(searchTerm.toLowerCase());
+  });
 
   return (
     <div className="products-list__container">
+
+      <input
+        type="text" 
+        name="search" 
+        className='products-list__search'
+        placeholder='Search Products by Name'
+        onChange={handleSearch}
+      />
+
       {
-        products.map(product => {
+        products.length < 1 && <h1>No Items in Database</h1>
+      }
+      {
+        products.length > 0 && searchTerm.length > 0 && <h1>Results for search: "{searchTerm}"</h1>
+      }
+      {
+        filteredProducts.map(product => {
           return (
             <ProductsItem key={product._id} product={product}/>
           )
         })
       }
+
     </div>
   )
 }
