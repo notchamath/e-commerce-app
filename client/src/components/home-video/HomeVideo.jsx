74c08f3e6ee6cbefd1b00779f0a6d93da3a77a76
Chamath Jayarathne <chamathsocial@gmail.com>
2023-02-27 15:58:04 -0500

    fix: Video autoplay fix

--- client/src/components/home-video/HomeVideo.jsx
+++ client/src/components/home-video/HomeVideo.jsx
@@ -20,6 +20,7 @@ export default function HomeVideo({product}) {
         autoPlay
         muted
         playsInline
+        defaultMuted
       >
         <source src={homeVideo} type="video/mp4" />
       </video>
