8d70a774cc8fb032f05c635d2062824db51b2ff1
Chamath Jayarathne <chamathsocial@gmail.com>
2023-02-27 14:48:31 -0500

    fix: Style 404 page

--- client/src/routes/not-found/PageNotFound.jsx
+++ client/src/routes/not-found/PageNotFound.jsx
@@ -1,13 +1,18 @@
 import { useNavigate } from "react-router-dom";
+import { BUTTON_TYPES } from '../../components/button/Button';
+
+import Button from '../../components/button/Button';
+
+import './PageNotFound.scss';
 
 export default function PageNotFound() {
   const navigate = useNavigate();
   const goBack = () => navigate(-1);
 
   return (
-    <>
-      <div>Page Not Found</div>
-      <button onClick={goBack}>Go Back</button>
-    </>
+    <section className="page-not-found__container">
+      <h1>Page Not Found :( </h1>
+      <Button buttonType={BUTTON_TYPES.PRIMARY} onClick={goBack}>Go Back</Button>
+    </section>
   )
 }
