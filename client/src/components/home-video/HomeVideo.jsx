71843a8c79664e625de5d6864948afe112c98b7d
Chamath Jayarathne <chamathsocial@gmail.com>
2023-02-27 10:29:13 -0500

    fix: Video preload change

--- client/src/components/home-video/HomeVideo.jsx
+++ client/src/components/home-video/HomeVideo.jsx
@@ -1,4 +1,3 @@
-import {useRef, useEffect} from "react"
 import { useNavigate } from 'react-router-dom';
 
 import homeVideo from '../../assets/videos/homeVideo.mp4';
@@ -6,13 +5,9 @@ import homeVideo from '../../assets/videos/homeVideo.mp4';
 import './HomeVideo.scss';
 
 export default function HomeVideo({product}) {
-  const videoRef = useRef();
   const navigate = useNavigate();
-
-  useEffect(() => {
-    videoRef.current.defaultMuted = true;
-  });
   
+  // handle click
   const handleClick = () => {
     navigate(`/products/${product?._id}`);
   }
@@ -21,12 +16,10 @@ export default function HomeVideo({product}) {
     <section className='home-video__container'>
       <h1 onClick={handleClick}>{product?.name}</h1>
       <video
-        ref={videoRef}
         loop
         autoPlay
         muted
         playsInline
-        preload='auto'
       >
         <source src={homeVideo} type="video/mp4" />
       </video>
