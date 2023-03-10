07d45c7a7d067b0536a4ded4fb9e4e25dad4a78f
Chamath Jayarathne <chamathsocial@gmail.com>
2023-03-10 13:13:33 -0500

    fix: Make home video playable when clicked

--- client/src/components/home-video/HomeVideo.jsx
+++ client/src/components/home-video/HomeVideo.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import homeVideo from '../../assets/videos/homeVideo.mp4';
@@ -6,23 +7,34 @@ import './HomeVideo.scss';
 
 export default function HomeVideo({product}) {
   const navigate = useNavigate();
+  const videoRef = useRef();
   
-  // handle click
+  // handle click on title
   const handleClick = () => {
     navigate(`/products/${product?._id}`);
+  }
+
+  // handle click on video
+  const handlePlay = () => {
+    videoRef.current.play();
   }
 
   return (
     <section className='home-video__container'>
       <h1 onClick={handleClick}>{product?.name}</h1>
-      <video
-        loop
-        autoPlay
-        muted
-        playsInline
-      >
-        <source src={homeVideo} type="video/mp4" />
-      </video>
+
+      <div className='home-video__video' onClick={handlePlay}>
+        <video
+          ref={videoRef}
+          loop
+          muted
+          playsInline
+          autoPlay
+        >
+          <source src={homeVideo} type="video/mp4" />
+        </video>
+      </div>
+
     </section>
   )
 }
